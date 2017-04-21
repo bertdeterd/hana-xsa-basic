13672dc318ea370baf754f3b6df1a48fde9e6579
bertdeterd <bertdeterd@creetion.com>
2017-04-21 19:58:51 +0200

    server settings

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## Create a service on XSA
 > xs create-service hana hdi-shared xsabasic-hdi-container
+> xs create-service xsuaa default xsabasic-uaa -c xs-security.json
 
---
+++ js/server.js
@@ -0,0 +1,26 @@
+/*eslint no-console: 0, no-unused-vars: 0*/
+"use strict";
+
+const xsjs  = require("sap-xsjs");
+const xsenv = require("sap-xsenv");
+const port  = process.env.PORT || 3000;
+const server = require("http").createServer();
+
+const init = require("./initialize");
+//const node = require("./myNode");
+const userRouter = require("./api/users/users_api");
+
+const options = {
+	//anonymous : true, // remove to authenticate calls
+	redirectUrl : "/index.xsjs"
+};
+
+const app = init.initExpress();
+
+app.use("/api", userRouter);
+
+init.initXSJS(app);
+
+// start server
+server.on("request", app);
+server.listen(port, () => console.info("Server listening on port %d", port))
